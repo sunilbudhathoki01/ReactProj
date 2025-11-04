5e9a61728dc92b15d9ea6b4296f206432d19bcae
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2025-11-04 16:20:36 +0545

    NEXT js project setup

---
+++ my-app/src/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = () => {
+  const Mapprac=()=>{
+    const num=[1,2,3,4]
+    var num1=num.map((n)=>n*n)
+    console.log(num1)
+  }
+  return (
+    <div style={{border:"1px solid",padding:"1rem"}}>       <p>Card</p>
+      <p>whataa</p>
+      <p>
+      </p>
+
+    </div>
+  )
+}
+
+export default Card
